782f2cc93059409b6378e240d2ca3d4ff136763d
Narendra Singh dangi <ns172128561@gmail.com>
2022-09-16 00:41:12 +0530

    camp added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import donarRoute from "./routes/donar.js"
+import campRoute from "./routes/camp.js"
 // import authRoute from "./routes/auth.js";
 // import ngosRoute from "./routes/ngo.js";
 
@@ -31,6 +32,7 @@ mongoose.connection.on("disconnected", () => {
 app.use(express.json());
 app.use(cors());
 app.use("/api/donar", donarRoute);
+app.use("/api/camp", campRoute);
 // app.use("/api/auth", authRoute);
 // app.use("/api/ngo", ngosRoute);
 
